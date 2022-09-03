08a6173161e200cd9d47fb378748e1c0659f76c5
Serafion <pzakrzewski87@gmail.com>
2022-09-03 20:41:01 +0200

    Introduced interfaces for services and BaseEntity class

---
+++ petclinicdata/src/main/java/pl/petclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package pl.petclinic.services;
+
+import pl.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ petclinicdata/src/main/java/pl/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package pl.petclinic.services;
+
+import pl.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
